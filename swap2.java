626120cbdf638a317eed630a5a070cfee2fd9ed9
Mahant <146162280+Mac1718@users.noreply.github.com>
2024-08-26 19:22:27 +0530

    Add files via upload

---
+++ swap2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main
+{
+  public static void main(String[] args)
+  {
+    Scanner n=new Scanner(System.in);
+    String str=n.nextLine();
+    StringBuilder d=new StringBuilder();
+    for(char ch:str.toCharArray())
+    {
+      if(Character.isLowerCase(ch))
+      {
+    break;
+  }
+  d.append(ch);
+}
+  System.out.print(d.toString());
+}
+}
