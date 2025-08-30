d6d1050915d8b9c60bcc356e98ba79208b717746
Mahant <146162280+Mac1718@users.noreply.github.com>
2025-08-30 12:07:26 +0530

    Add files via upload

---
+++ Object1.java
@@ -0,0 +1,15 @@
+package Objects;
+
+
+
+public class Object1 
+{
+    public static void main(String[] args) {
+       
+    }    
+
+     public void test()
+        {
+            System.out.println("This is test() of demo class");
+        }
+}
